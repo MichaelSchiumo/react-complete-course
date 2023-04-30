c38330a015d9e36d3e378264d20e785e7fce5c31
Michael J. Schiumo <mschiumo@simulmedia.com>
2023-04-30 14:56:13 -0400

    css and basic component

--- react-course/src/components/ExpenseItem.js
+++ react-course/src/components/ExpenseItem.js
@@ -1,5 +1,20 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
-  return <h2>Custom Expense Component</h2>;
+
+  const expenseDate = new Date(2021, 2, 29).toISOString();
+  const title = 'Expense';
+  const expenseAmount = 230.00;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  )
 }
 
 export default ExpenseItem;
