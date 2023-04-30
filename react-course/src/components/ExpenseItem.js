8ab615d2ef8fcd6ab614a6e498f1777048839f84
Michael J. Schiumo <mschiumo@simulmedia.com>
2023-04-30 15:33:34 -0400

    added expense date component and g react component

--- react-course/src/components/ExpenseItem.js
+++ react-course/src/components/ExpenseItem.js
@@ -1,17 +1,12 @@
 import './ExpenseItem.css'
+import './ExpenseDate.js';
+import ExpenseDate from "./ExpenseDate.js";
 
 function ExpenseItem(props) {
-  
-  const month = props.date.toLocaleString('en-us', { month: 'long' });
-  const day = props.date.toLocaleString('en-us', { day: '2-digit' });
-  const year = props.date.getFullYear();
 
-  console.log(props);
   return (
     <div className="expense-item">
-      <div>{month}{' '}</div>
-      <div>{day}, {' '}</div>
-      <div>{year}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
