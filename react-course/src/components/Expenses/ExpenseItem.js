e83a91fad37bb811f28ea0b66f8b21ee80a8860d
Michael J. Schiumo <mschiumo@simulmedia.com>
2023-05-15 21:54:59 -0400

    use state original

--- react-course/src/components/Expenses/ExpenseItem.js
+++ react-course/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,24 @@
-import './ExpenseItem.css'
+import React, { useState } from 'react'; 
+import './ExpenseItem.css';
 import './ExpenseDate.js';
 import ExpenseDate from "./ExpenseDate.js";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('updated');
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button type="button" onClick={clickHandler}>Change Title</button>
     </Card>
   )
 }
