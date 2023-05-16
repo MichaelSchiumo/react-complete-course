9b81aa61204407bc7e3eeab01721773bfed2ef60
Michael J. Schiumo <mschiumo@simulmedia.com>
2023-05-15 21:11:41 -0400

    fix css

--- react-course/src/components/Expenses/ExpenseDate.js
+++ react-course/src/components/Expenses/ExpenseDate.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ExpenseDate.css';
 
 
 function ExpenseDate(props) {
@@ -7,10 +8,10 @@ function ExpenseDate(props) {
   const year = props.date.getFullYear();
 
   return (
-    <div>
-      <div>{month}</div>
-      <div>{day}</div>
-      <div>{year}</div>
+    <div className='expense-date'>
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__year'>{year}</div>
+      <div className='expense-date__day'>{day}</div>
     </div>
   )
   };
