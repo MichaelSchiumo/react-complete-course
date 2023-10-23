c69c9efd92bda2cd84b999a3e03b5515baad2285
Michael <mschiumo18@gmail.com>
2023-10-22 21:20:10 -0400

    add styled component package

--- starting-project-2/src/components/UI/Button/Button.js
+++ starting-project-2/src/components/UI/Button/Button.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import './Button.css';
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
+
+//styled.button`` is a tagged template literal
+//default JS feature
+//button is a method on styled that is called using the backticks
+//what is passed into the backticks will be used in the method
+const Button = styled.button``;
 
 export default Button;
